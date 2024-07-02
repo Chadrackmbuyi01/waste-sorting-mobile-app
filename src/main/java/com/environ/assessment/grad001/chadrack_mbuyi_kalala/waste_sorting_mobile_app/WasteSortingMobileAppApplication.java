acd771140bf9224f2c4aa7468a85a81c84c824a9
Chadrack Mbuyi Kalala <80404784+Chadrackmbuyi01@users.noreply.github.com>
2024-07-02 13:48:50 +0200

    First commit

---
+++ src/main/java/com/environ/assessment/grad001/chadrack_mbuyi_kalala/waste_sorting_mobile_app/WasteSortingMobileAppApplication.java
@@ -0,0 +1,13 @@
+package com.environ.assessment.grad001.chadrack_mbuyi_kalala.waste_sorting_mobile_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteSortingMobileAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteSortingMobileAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/environ/assessment/grad001/chadrack_mbuyi_kalala/waste_sorting_mobile_app/WasteSortingMobileAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.environ.assessment.grad001.chadrack_mbuyi_kalala.waste_sorting_mobile_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteSortingMobileAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
